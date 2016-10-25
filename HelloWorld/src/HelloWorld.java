02800115bc269a66a6ac3e6f41d44fe4f3a87a3d
Rakesh <Rakesh@DESKTOP-TMTJV3C>
2016-10-25 16:22:49 -0700

    HelloWorld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	void display() {
+		System.out.println("Jenkins");
+
+	}
+
+	public static void main(String args[]) {
+		HelloWorld jenk = new HelloWorld();
+		jenk.display();
+
+	}
+}
